f2779569acb519b911910da40fa80d844f4d248d
117119949@163.com <117119949@163.com>
2025-12-08 16:07:02 +0800

    指针

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"golang_study/task1"
+	"golang_study/task2"
 )
 
 func main() {
@@ -17,5 +18,12 @@ func main() {
 	// 	3, 3,
 	// }
 	target := 6
-	fmt.Println(task1.TwoSum(nums, target))
+	sum, ok := task1.TwoSum(nums, target)
+	if ok {
+		fmt.Println("两数之和", sum)
+	}
+
+	//指针
+	number := 5
+	fmt.Println("指针", task2.ChangeNumber(&number))
 }
