ffc8d75b34aa9fc633e9841ebf99998b07817c84
117119949@163.com <117119949@163.com>
2025-12-08 15:22:40 +0800

    两数之和

---
+++ task1/TwoSum.go
@@ -0,0 +1,16 @@
+package task1
+
+func TwoSum(nums []int, target int) ([2]int, bool) {
+	flag := false
+	var array [2]int
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if int(nums[i]+nums[j]) == target {
+				array[0] = i
+				array[1] = j
+				flag = true
+			}
+		}
+	}
+	return array, flag
+}
