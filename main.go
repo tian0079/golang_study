465e564ddfa72094a3082ccb9bbf17b796368ff4
117119949@163.com <117119949@163.com>
2025-12-08 16:01:06 +0800

    指针

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"golang_study/task1"
+)
+
 func main() {
 
+	// nums := []int{
+	// 	2, 7, 11, 15,
+	// }
+	nums := []int{
+		3, 2, 4,
+	}
+	// nums = []int{
+	// 	3, 3,
+	// }
+	target := 6
+	fmt.Println(task1.TwoSum(nums, target))
 }
